1b715b675a0feaf6063b51c02a42d373bfd6cd3b
Detroit98 <duruakuebuka@gmail.com>
2023-04-05 14:05:56 +0100

    app

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,34 +5,36 @@ import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalCompon
 import { projects } from '../../constants/constants';
 
 const Projects = () => (
- <Section nopadding id="projects">
-  <SectionDivider />
-  <SectionTitle main>Projects</SectionTitle>
-  <GridContainer>
-    {projects.map(({ title,description,image,tags,source,visit,id})=>(
-    <BlogCard key = {id}>
-      <Img src={image} />
-      <TitleContent>
-        <HeaderThree title>{title} </HeaderThree>
-        <Hr />
-      </TitleContent>
-      <CardInfo>{description}</CardInfo>
-      <div>
-        <TitleContent>Stack</TitleContent>
-        <TagList>
-          {tags.map((tag,i)=>(
-            <Tag key={i}>{tag}</Tag>
-          ))}
-        </TagList>
-      </div>
-      <UtilityList>
-        <ExternalLinks href={visit}>Code</ExternalLinks>
-        <ExternalLinks href={source}>Source</ExternalLinks>
-      </UtilityList>
-    </BlogCard>
-    ))}
-  </GridContainer>
- </Section>
+  <Section nopadding id="projects">
+    <SectionDivider />
+    <SectionTitle main>Projects</SectionTitle>
+    <GridContainer>
+      {projects.map(
+        ({ title, description, image, tags, source, visit, id }) => (
+          <BlogCard key={id}>
+            <Img src={image} />
+            <TitleContent>
+              <HeaderThree title>{title} </HeaderThree>
+              <Hr />
+            </TitleContent>
+            <CardInfo>{description}</CardInfo>
+            <div>
+              <TitleContent>Stack</TitleContent>
+              <TagList>
+                {tags.map((tag, i) => (
+                  <Tag key={i}>{tag}</Tag>
+                ))}
+              </TagList>
+            </div>
+            <UtilityList>
+              <ExternalLinks href={visit}>App</ExternalLinks>
+              <ExternalLinks href={source}>Source</ExternalLinks>
+            </UtilityList>
+          </BlogCard>
+        )
+      )}
+    </GridContainer>
+  </Section>
 );
 
 export default Projects;
